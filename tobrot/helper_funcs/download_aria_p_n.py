7fe21b3320f43ffe30b46ce0fcde437129c301e6
JNS <65025344+jinto4638@users.noreply.github.com>
2021-05-06 07:17:09 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -332,11 +332,11 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
             await check_progress_for_dl(aria2, gid, event, previous_message)
         else:
             LOGGER.info(
-                f"<b> Downloaded Successfully ✨  \n\n file Name 📁🗂 :- </b> `{file.name}` \n\n <b> file size🤯 - {file.total_length_string()} </b>"
+                f"<b> Downloaded Successfully ✨  \n\n file Name 📁🗂 :- </b> `{file.name}` \n\n <b> file size - </b> {file.total_length_string()}🙊"
             )
             await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
             await event.edit(
-                f"<b> Downloaded Successfully ✨  \n\n file Name 📁🗂 :- </b> `{file.name}` \n\n <b> file size🤯 - {file.total_length_string()} </b>"
+                f"<b> Downloaded Successfully ✨  \n\n file Name 📁🗂 :- </b> `{file.name}` \n\n <b> file size - </b> {file.total_length_string()}🙊"
             )
             return True
     except aria2p.client.ClientException:
